de04ae0997115d4cc78374c05b3495c6937b61bc
Zidaan <zizouhabib@gmail.com>
2020-10-20 22:41:34 +0200

    Added distance matrix api functionality and began configuration methods

--- agro_kit/gps.py
+++ agro_kit/gps.py
@@ -2,11 +2,18 @@ import json
 import serial
 from time import sleep
 import pynmea2 as nmea
+import requests
 
 class GPS:
 
+    BAUD_RATES = [4800, 9600, 14400, 19200, 38400, 57600, 115200]
+    BAUD_RATE_CHKSUMS = {'4800': "48", '9600': '4B', '14400': '75', '19200': '7E', '38400': '7B', '57600': '70', '115200': '43'}
+
+
+
     #Static Var
     ser = serial.Serial ("/dev/ttyS0", 9600 , timeout=1)
+    API_KEY = '' # empty by default
 
 
     def __init__(self, baud_rate = 9600, timeout = 5):
@@ -16,6 +23,24 @@ class GPS:
         self.ser.baudrate = baud_rate
         self.ser.timeout = timeout
 
+
+#######################################################################################3
+#Configuration methods
+#######################################################################################3
+
+#set baud rate of GPS receiver:
+
+#note that only baud rates of 4800, 9600, 14400, 19200, 38400, 57600, 115200 are allowed
+    def setGPSBaudRate(self, baud_rate):
+        if baud_rate in GPS.BAUD_RATES:
+            str_baud_rate = str(baud_rate)
+            msg = '$PQBAUD,W,' + str_baud_rate + '*' + GPS.BAUD_RATE_CHKSUMS[str_baud_rate] + '\n'
+            ser.write(msg.encode())
+            print(ser.readline().decode())
+
+
+#########################################################################
+#Methods to get different NMEA message types from GPS module:
 
     #recommended minimum position data
     def getRMC(self):
@@ -66,6 +91,35 @@ class GPS:
         nmea_obj = nmea.parse(nmea_msg)
         return nmea_obj
 
+#######################################################################################3
+#Methods for the Distance Matrix API:
+#######################################################################################3
+
+
+    def distanceTo(self, dst_lat, dst_long):
+        data = self.getGLL()
+        origin_lat = data.latitude
+        origin_long = data.longitude
+        return GPS.distanceFromTo(origin_lat, origin_long, dst_lat, dst_long)
+
+    #set Google API key:
+    @classmethod
+    def setAPIKey(cls, key):
+        API_KEY = key
+
+
+    #Calculate distance from specified coords to destination coords in meters:
+    @classmethod
+    def distanceFromTo(cls, origin_lat, origin_long, dst_lat, dst_long):
+        #if origin_lat.isNumeric() and origin_lat.isNumeric() and isNumeric(dst_lat) and isNumeric(dst_long):
+            url1 = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+            url2 = 'origins='+str(origin_lat) + ',' + str(origin_long)
+            url3 = '&destinations=' +str(dst_lat) + ',' + str(dst_long)
+            url4 = "&mode=car&key=" + API_KEY
+            url = url1 + url2 + url3 + url4
+            output = requests.get(url).json() #sending get request to google maps Distance MatriX API
+            return output["rows"][0]["elements"][0]["distance"]["value"]
+
 
 
 
@@ -73,7 +127,9 @@ class GPS:
 #used for testing
 if __name__ == "__main__":
     myGPS = GPS()
-    x = myGPS.getRMC()
-    print(x.latitude + "\n")
-    y = myGPS.getGLL()
-    print(x.latitude + "\n")
+    with open("../.gmaps_key", "r") as f:
+        API_KEY = f.readline().strip()
+        f.close()
+    GPS.setAPIKey(API_KEY)
+    dst_lat, dst_long = -26.146446, 28.041632
+    myGPS.setGPSBaudRate(4800)
