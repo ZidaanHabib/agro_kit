65f05ef1b8389739e4a66227bf5af1be4c98716c
frasermo <51508635+frasermo@users.noreply.github.com>
2020-10-30 15:29:49 +0200

    light sensor- single read update

--- agro_kit/light_sensor.py
+++ agro_kit/light_sensor.py
@@ -8,8 +8,11 @@ import adafruit_tcs34725
 import RPi.GPIO as GPIO
 import json
 
+# default values:
 temp=  0
 sample_rate= 0
+pwr_pin = 17
+
 class light_sensor:
 
     def __init__(self, datapin, pwr_pin  ):
@@ -24,6 +27,9 @@ class light_sensor:
         self.sample_rate = 5
 
         self.light_sensor_config = adafruit_tcs34725.TCS34725(i2cbus)
+    
+    def set_pwr_pin(self,pin_num):
+        self.pwr_pin = pin_num
 
     def powerUp(self, pwr_pin):
          # GPIO PIN  to power sensor
@@ -53,14 +59,14 @@ class light_sensor:
 
 
 
-    def singleReadLux(self, pwr_pin):
+    def singleReadLux(self):
         self.powerUp(pwr_pin)
         lux = self.light_sensor_config.lux
         self.powerDown(pwr_pin)
         #print(lux)
         return lux
 
-    def singleReadColour(self, pwr_pin):
+    def singleReadColour(self):
         self.powerUp(pwr_pin)
         colour = self.light_sensor_config.color_temperature
         self.powerDown(pwr_pin)
@@ -107,12 +113,13 @@ class light_sensor:
 
 if __name__ == "__main__":
     light_sensor_test = light_sensor(17, 17)
+    light_sensor_test.singleReadLux()
 
-    '''light_sensor_test.RGB(17)
+    #'''light_sensor_test.RGB(17)
     light_sensor_test.lux(17)
-    light_sensor_test.powerUp(17)
+    #light_sensor_test.powerUp(17)
     light_sensor_test.colour(17)
-    light_sensor_test.singleReadColour(17)
-    light_sensor_test.powerDown(17)'''
+    #light_sensor_test.singleReadColour(17)
+    light_sensor_test.powerDown(17)
 
-    print(light_sensor_test.singleReadLux(17))
+
