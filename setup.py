1ea9fbbbb8e72f201881c39d5de5f34b49a9c17b
Zidaan <zizouhabib@gmail.com>
2020-11-06 21:39:52 +0200

    Changed setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
-
-setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest>=3', ]
 
